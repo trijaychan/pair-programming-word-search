d5ad3b45ac098896e715e8449fae774e66ef2da3
trijaychan <your@email.com>
2022-02-17 03:41:52 +0000

    working

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,53 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let temp;
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        temp = l.split("").reverse().join("");
+        if (l.includes(word) || temp.includes(word)) return true;
     }
-}
 
-module.exports = wordSearch
+    let verticalJoin = [];
+
+    for (let i = 0; i < letters[0].length; i++) {
+        verticalJoin.push([""]);
+        for (let j = 0; j < letters.length; j++) {
+            verticalJoin[i] += letters[j][i];
+        }
+
+        temp = verticalJoin[i].split("").reverse().join("");
+
+        if (verticalJoin[i].includes(word) || temp.includes(word)) return true;
+    }
+
+    return false;
+};
+
+// function wordSearch(letters, word) {
+
+//     for (let j = 0 ; j < letters[0].length ; j++) {
+
+//         let verticalWord = '';
+//         let horizontalWord = '';
+
+//         for (let i = 0 ; i < letters.length ; i++) {
+//             verticalWord += letters[i][j];
+//             horizontalWord += letters[j][i];
+//         }
+
+//         if ((verticalWord.includes(word)) ||
+//             (verticalWord.split('').reverse().join('').includes(word)))
+//         {
+//             return true;
+//         }
+//         if ((horizontalWord.includes(word)) ||
+//             (horizontalWord.split('').reverse().join('').includes(word)))
+//         {
+//             return true;
+//         }
+//     }
+
+//      return false;
+
+// }
+
+module.exports = wordSearch;
